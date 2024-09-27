17f5ee4a96c2382404a8d8bab33305dc9f7c64b4
Akashdeep <akash.kashhyap@gmail.com>
2024-09-27 14:55:51 +0530

    update style

--- components/HomeServiceLead.js
+++ components/HomeServiceLead.js
@@ -8,17 +8,17 @@ export default function HomeServiceLead({ blok }) {
 
   return (
     <section
-      className="bg-[#e5e5e5] pt-12 lg:pt-24 px-4 home_service"
+      className="bg-[#e5e5e5] pt-16 lg:pt-24 px-4 home_service"
       {...storyblokEditable(blok)}
     >
-      <div className="fluid_container container grid grid-cols-12 items-center">
+      <div className="fluid_container container grid grid-cols-12 lg:items-center">
         {/* Left Text Section */}
-        <div className="homeservice_description col-span-full 2xl:col-span-5 text-center lg:text-left max-w-lg">
+        <div className="homeservice_description col-span-full 2xl:col-span-5 text-center lg:text-left 2xl:max-w-lg">
           {render(blok?.description)}
         </div>
 
         {/* Center Image Section */}
-        <div className="col-span-full xl:col-span-6 2xl:col-span-4 order-3 xl:order-2 h-full w-full lg:w-auto flex justify-end">
+        <div className="col-span-full lg:col-span-6 order-3 lg:order-2 h-full w-full lg:w-auto flex justify-end">
           <div className="relative w-full aspect-h-9 aspect-w-16">
             {blok?.image.filename && (
               <Image
@@ -32,7 +32,7 @@ export default function HomeServiceLead({ blok }) {
         </div>
 
         {/* Right Text Section */}
-        <div className="homeservice_rt_content col-span-full xl:col-span-6 2xl:col-span-3 order-2 xl:order-3 mb-16 xl:mb-0">
+        <div className="homeservice_rt_content col-span-full lg:col-span-6 order-2 lg:order-3 mb-16">
           {render(blok?.content)}
           <a
             href={blok?.button_link.cached_url}
