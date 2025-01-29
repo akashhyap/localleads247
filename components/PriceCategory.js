3a9e3c050426561e64305f2a1bc2a33e9ee03070
Akashdeep <akash.kashhyap@gmail.com>
2025-01-29 19:25:38 +0530

    update U pricingI

--- components/PriceCategory.js
+++ components/PriceCategory.js
@@ -5,9 +5,9 @@ const Pricing = ({ blok }) => {
 
   return (
     <div {...storyblokEditable(blok)} className="relative mb-10 mt-5 lg:mt-2">
-      <div className="mb-5"><span className="eyebrow_text">{blok.preTitle}</span></div>
+      <div><span className="eyebrow_text">{blok.preTitle}</span></div>
       <h2>{blok.title}</h2>
-      <div className="priceLevels grid grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="priceLevels grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {blok.priceLevels.map((nestedBlok) => (
           <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
--- components/PriceLevel.js
+++ components/PriceLevel.js
@@ -5,10 +5,10 @@ const PriceLevel = ({ blok }) => {
 
   return (
     <div {...storyblokEditable(blok)} className="priceLevel bg-white rounded-xl p-8 mb-10 mt-5 lg:mt-2">
-      <p>{blok.preTitle}</p>
-      <p className="text-4xl font-bold mt-4">{blok.price}</p>
-      <p>{blok.priceTenure}</p>
-      <div className="mt-5 [&>ul]:list-disc [&>ul]:text-lg [&>ul>li]:leading-relaxed [&>ul>li]:mb-3">{render(blok?.description)}</div>
+      <p className="text-lg">{blok.preTitle}</p>
+      <p className="text-4xl font-bold mt-4 text-[#38b5fe]">{blok.price}</p>
+      <p className="text-lg">{blok.priceTenure}</p>
+      <div className="mt-5 [&>ul]:list-disc [&>ul]:pl-4 [&>ul]:text-lg [&>ul>li]:leading-relaxed [&>ul>li]:mb-3">{render(blok?.description)}</div>
     </div>
   );
 };
